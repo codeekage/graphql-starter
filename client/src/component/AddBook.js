24105432f1c281717fb09d32ef6a3912eb1b7f2f
codeekage <agiriabrahamjunior@gmail.com>
2018-08-02 07:25:21 +0100

    added queries

--- client/src/component/AddBook.js
+++ client/src/component/AddBook.js
@@ -1,16 +1,7 @@
 import React, { Component } from 'react';
-import { gql } from "apollo-boost";
 import { graphql } from 'react-apollo'
+import { getAuthorsQuery } from "../query";
 
-
-const getAuthorsQuery = gql`
-    {
-        authors{
-            name
-            id
-        }
-    }
-`
 class AddBooks extends Component {
 
     displayAuthor() {
